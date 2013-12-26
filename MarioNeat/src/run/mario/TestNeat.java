497828e51f2b872803404687caa90d6aefbb314b
huiwq1990 <huiwq1990@163.com>
2013-12-26 18:03:02 +0800

    run neat

---
+++ MarioNeat/src/run/mario/TestNeat.java
@@ -0,0 +1,13 @@
+package run.mario;
+
+
+public class TestNeat {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		String[] parms = {"xor.properties",""};		
+//		Evaluator.main(parms);
+	}
+
+}
