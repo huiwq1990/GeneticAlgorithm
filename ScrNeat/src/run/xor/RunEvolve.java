f8e39163f194bf0a73a45e361d8c9d70961aac93
huiwq1990 <huiwq1990@163.com>
2014-01-07 17:30:32 +0800

    s

---
+++ ScrNeat/src/run/xor/RunEvolve.java
@@ -0,0 +1,13 @@
+package run.xor;
+
+import com.anji.neat.Evolver;
+
+public class RunEvolve {
+
+	public static void main(String[] args) throws Throwable {
+		// TODO Auto-generated method stub
+		String[] parms = {"torcs.properties"};
+		Evolver.main(parms);
+	}
+
+}
